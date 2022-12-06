e8459108c0f3e4814e09c3a390e5e2745dc041f9
Marcelito Cosicol <mcosicoljr@gmail.com>
2022-12-06 19:06:59 +0800

    Setup Four Micro-frontends, done setup tailwind in Compontents App

---
+++ components/pages/dev2.jsx
@@ -0,0 +1,17 @@
+export default function Dev2() {
+    return (
+      <div className="flex justify-center mt-20 flex-col items-center gap-10">
+        <h1 className="text-3xl font-bold text-center">
+          Hello Kyle!
+        </h1>
+        <p className="text-xl" >
+         This page is the compiled finished UI COMPONENTS and Functional Components.
+         Please put the following: <br/>
+         1.) Name of the component <br/>
+         2.) The Component <br/>
+         3.) The list of its variants
+        </p>
+      </div>
+
+    )
+  }
